4c77fd72c3f676acbef7147ac367583e79daf677
Xie Benyi <xieby1@outlook.com>
2021-07-23 10:26:19 +0800

    mv run.sh to scripts, add sample rate option

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+# exit when command failed
+set -e
+
+# variables
+PPUA_DEFAULT_SAMPLE_RATE=65536
+
 ${PIN_ROOT:?"not set"}/pin \
     -t ${PPUA_ROOT:?"not set"}/pintoolMemTracer/obj-intel64/memTracer.so \
+    -r ${PPUA_SAMPLE_RATE:-${PPUA_DEFAULT_SAMPLE_RATE}} \
     -- \
     $@
 
